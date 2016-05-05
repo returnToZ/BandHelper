a2ba5eb3b6663de4efe9880b88832849f9b76355
James <caterpillarous@yahoo.com>
2016-05-05 22:21:01 +0800

    init

---
+++ app/src/main/java/james/com/mag1c_band/MainActivity.java
@@ -0,0 +1,13 @@
+package james.com.mag1c_band;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/james/com/mag1c_band/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package james.com.mag1c_band;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
