1f011c8cae4a7fab4e8650a0e443559af434489a
James <caterpillarous@yahoo.com>
2016-05-07 20:04:23 +0800

    UI update

---
+++ app/src/main/java/james/com/mag1c_band/PersonalInfoActivity.java
@@ -0,0 +1,13 @@
+package james.com.mag1c_band;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PersonalInfoActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info);
+    }
+}
--- app/src/main/java/james/com/mag1c_band/SettingActivity.java
+++ app/src/main/java/james/com/mag1c_band/SettingActivity.java
@@ -1,12 +1,27 @@
 package james.com.mag1c_band;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TableRow;
 
-public class SettingActivity extends Activity{
+public class SettingActivity extends Activity implements View.OnClickListener{
+    TableRow personInfo;
     @Override
-    protected void onCreate(Bundle savedInstaceState){
-        super.onCreate(savedInstaceState);
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        personInfo = (TableRow)findViewById(R.id.personInfo);
+        personInfo.setOnClickListener(this);
+    }
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.personInfo:
+                Intent intent1 = new Intent(v.getContext(),PersonalInfoActivity.class);
+                startActivity(intent1);
+        }
     }
 }
