3b121c50441d84800fdc9c15a5bf3797fb3c17f8
James <caterpillarous@yahoo.com>
2016-05-27 10:46:28 +0800

    update

--- app/src/main/java/james/com/mag1c_band/UI/RegisterActivity.java
+++ app/src/main/java/james/com/mag1c_band/UI/RegisterActivity.java
@@ -1,14 +1,37 @@
 package james.com.mag1c_band.UI;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import james.com.mag1c_band.R;
 
-public class RegisterActivity extends Activity {
+public class RegisterActivity extends Activity{
+    Button register;
+    Button cancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        register = (Button)findViewById(R.id.register);
+        cancel = (Button)findViewById(R.id.cancel);
+        register.getBackground().setAlpha(0);
+        cancel.getBackground().setAlpha(0);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
