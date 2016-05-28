4639a9a5c4e64ef3cfb8760003e10ba2ec6fe3c9
James <caterpillarous@yahoo.com>
2016-05-28 14:06:04 +0800

    now can share sth,replace AppKey on your own

--- app/src/main/java/james/com/mag1c_band/UI/MainActivity.java
+++ app/src/main/java/james/com/mag1c_band/UI/MainActivity.java
@@ -2,10 +2,19 @@ package james.com.mag1c_band.UI;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.editorpage.ShareActivity;
+import com.umeng.socialize.media.UMImage;
 
 import james.com.mag1c_band.R;
 
@@ -14,11 +23,22 @@ public class MainActivity extends Activity {
     ImageView setting;
     ImageView share;
     ImageView chat;
+    final SHARE_MEDIA[] displaylist = new SHARE_MEDIA[]
+            {
+                    SHARE_MEDIA.WEIXIN, SHARE_MEDIA.WEIXIN_CIRCLE,SHARE_MEDIA.SINA,
+                    SHARE_MEDIA.QQ, SHARE_MEDIA.QZONE,SHARE_MEDIA.DOUBAN
+            };
     public static MainActivity MainInstance = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
+        //微信 appid appsecret
+        PlatformConfig.setSinaWeibo("3319386979", "4f8927a364847eac97ccb8f11abb9ac5");
+        //新浪微博 appkey appsecret
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+        // QQ和Qzone appid appkey
         setting = (ImageView)findViewById(R.id.setting);
         share = (ImageView)findViewById(R.id.share);
         chat = (ImageView)findViewById(R.id.chat);
@@ -33,8 +53,37 @@ public class MainActivity extends Activity {
         chat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(),ChatActivity.class);
+                Intent intent = new Intent(view.getContext(), ChatActivity.class);
                 startActivity(intent);
+            }
+        });
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UMImage image = new UMImage(MainActivity.this,
+                        BitmapFactory.decodeResource(getResources(), R.drawable.icon_app));
+                new ShareAction(MainActivity.this).setDisplayList(displaylist)
+                        .withText( "test" )
+                        .withTitle("title")//title参数对新浪、人人、豆瓣不生效
+                        .withTargetUrl("Jameeeees.github.io")//文本中的目标地址
+                        .withMedia(image)
+                        .setListenerList(new UMShareListener() {
+                            @Override
+                            public void onResult(SHARE_MEDIA platform) {
+                                Toast.makeText(MainActivity.this, platform + " 分享成功啦", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onError(SHARE_MEDIA platform, Throwable t) {
+                                Toast.makeText(MainActivity.this, platform + " 分享失败啦", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onCancel(SHARE_MEDIA platform) {
+                                Toast.makeText(MainActivity.this, platform + " 分享取消了", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .open();
             }
         });
     }
---
+++ app/src/main/java/james/com/mag1c_band/WBShareActivity.java
@@ -0,0 +1,7 @@
+package james.com.mag1c_band;
+
+import com.umeng.socialize.media.WBShareCallBackActivity;
+
+public class WBShareActivity extends WBShareCallBackActivity {
+
+}
