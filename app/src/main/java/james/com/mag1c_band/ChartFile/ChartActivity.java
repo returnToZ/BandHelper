ea9698ecac720e5aba1c3b846094b77f6e3437dd
James <caterpillarous@yahoo.com>
2016-07-24 00:23:34 +0800

    dynamic chart

--- app/src/main/java/james/com/mag1c_band/ChartFile/ChartActivity.java
+++ app/src/main/java/james/com/mag1c_band/ChartFile/ChartActivity.java
@@ -1,123 +1,90 @@
 package james.com.mag1c_band.ChartFile;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.os.Handler;
+import android.os.Message;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 
-import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
-import org.achartengine.chart.PointStyle;
-import org.achartengine.model.XYMultipleSeriesDataset;
-import org.achartengine.model.XYSeries;
-import org.achartengine.renderer.XYMultipleSeriesRenderer;
-import org.achartengine.renderer.XYSeriesRenderer;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import james.com.mag1c_band.R;
-import james.com.mag1c_band.UI.ChatActivity;
-import james.com.mag1c_band.UI.ExitWindow;
 
 public class ChartActivity extends Activity {
-    private Button chart;
-    private String title = "线性统计图示例";
-    private XYSeries series;// XY数据点
-    private XYMultipleSeriesDataset mDataset;// XY轴数据集
-    private GraphicalView mViewChart;// 用于显示现行统计图
-    private XYMultipleSeriesRenderer mXYRenderer;// 线性统计图主描绘器
-    private Context context;// 用于获取上下文对象
-    private LinearLayout mLayout;
-    private int X = 5;// X数据集大小
-    private int Y = 5;//
+
+    private LinearLayout mLeftCurveLayout;//存放左图表的布局容器
+    private LinearLayout mRightCurveLayout;//存放右图表的布局容器
+    private GraphicalView mView, mView2;//左右图表
+    private ChartService mService, mService2;
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
-        chart = (Button) findViewById(R.id.chart);
-        startGraph();
-        chart.setOnClickListener(new View.OnClickListener() {
+
+        mLeftCurveLayout = (LinearLayout) findViewById(R.id.left_temperature_curve);
+        //mRightCurveLayout = (LinearLayout) findViewById(R.id.right_temperature_curve);
+
+        mService = new ChartService(this);
+        mService.setXYMultipleSeriesDataset("左温度曲线");
+        mService.setXYMultipleSeriesRenderer(100, 50, "左温度曲线", "时间", "温度",
+                Color.RED, Color.RED, Color.RED, Color.BLACK);
+        mView = mService.getGraphicalView();
+
+        /*
+        mService2 = new ChartService(this);
+        mService2.setXYMultipleSeriesDataset("右温度曲线");
+        mService2.setXYMultipleSeriesRenderer(100, 100, "右温度曲线", "时间", "温度",
+                Color.RED, Color.RED, Color.RED, Color.BLACK);
+        mView2 = mService2.getGraphicalView();
+        */
+        //将左右图表添加到布局容器中
+        mLeftCurveLayout.addView(mView, new LayoutParams(
+                LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        /*
+        mRightCurveLayout.addView(mView2, new LayoutParams(
+                LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+                */
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
             @Override
-            public void onClick(View view) {
-                //Intent intent = new Intent(view.getContext(), DynamicChart.class);
-                //startActivity(intent);
+            public void run() {
+                handler.sendMessage(handler.obtainMessage());
             }
-        });
+        }, 3000, 3000);
+    }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+*/
+    private int t = 0;
+    private Handler handler = new Handler() {
+        @Override
+        //定时更新图表
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            mService.updateChart(t, Math.random() * 100 % 50);
+            //mService2.updateChart(t, Math.random() * 100);
+            t+=5;
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (timer != null) {
+            timer.cancel();
+        }
     }
 
-    /*
-以下为设置图表的逻辑
- */
-    public void startGraph() {
-        context = getApplicationContext();// 获取上下文对象
-        mLayout = (LinearLayout) findViewById(R.id.chart_space);// 这里获得xy_chart的布局，下面会把图表画在这个布局里面
-        series = new XYSeries(title);// 这个类用来放置曲线上的所有点，是一个点的集合，根据这些点画出曲线
-
-        mDataset = new XYMultipleSeriesDataset(); // 创建一个数据集的实例，这个数据集将被用来创建图表
-        mDataset.addSeries(series);// 将点集添加到这个数据集中
-
-        int color = Color.RED;// 设置颜色
-        PointStyle style = PointStyle.CIRCLE;// 设置外观周期性显示
-        mXYRenderer = buildRenderer(color, style, true);
-        mXYRenderer.setShowGrid(true);// 显示表格
-        mXYRenderer.setGridColor(Color.GREEN);// 据说绿色代表健康色调，不过我比较喜欢灰色
-        mXYRenderer.setXLabels(5);//设置X轴刻度个数
-        mXYRenderer.setYLabels(5);//设置Y轴刻度个数
-        mXYRenderer.setExternalZoomEnabled(true);//设置是否可以缩放
-        mXYRenderer.setYLabelsAlign(Paint.Align.RIGHT);// 右对齐
-        mXYRenderer.setAxisTitleTextSize(30f); // 坐标轴标题字体大小： 16
-        mXYRenderer.setChartTitleTextSize(30f); // 图表标题字体大小： 20
-        mXYRenderer.setLabelsTextSize(30f); // 轴标签字体大小： 15
-        mXYRenderer.setLegendTextSize(30f); // 图例字体大小：
-        mXYRenderer.setShowLegend(false);// 不显示图例
-        mXYRenderer.setZoomEnabled(false);
-        mXYRenderer.setPanEnabled(true, false);
-        mXYRenderer.setClickEnabled(false);
-        setChartSettings(mXYRenderer, title, "时间", "数量", 1, 5, 50, 170,//xmin,xmax,ymin,ymax
-                Color.WHITE, Color.WHITE);// 这个是采用官方APIdemo提供给的方法
-        // 设置好图表的样式
-
-        mViewChart = ChartFactory.getLineChartView(context, mDataset,
-                mXYRenderer);// 通过ChartFactory生成图表
-
-        mLayout.addView(mViewChart, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT));// 将图表添加到布局中去
-    }
-
-    protected XYMultipleSeriesRenderer buildRenderer(int color,
-                                                     PointStyle style, boolean fill)
-    {// 设置图表中曲线本身的样式，包括颜色、点的大小以及线的粗细等
-        XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
-        XYSeriesRenderer r = new XYSeriesRenderer();
-        r.setColor(color);
-        r.setPointStyle(style);
-        r.setFillPoints(fill);
-        r.setLineWidth(3);
-        renderer.addSeriesRenderer(r);
-        return renderer;
-    }
-
-    protected void setChartSettings(XYMultipleSeriesRenderer renderer,
-                                    String title, String xTitle, String yTitle, double xMin,
-                                    double xMax, double yMin, double yMax, int axesColor,
-                                    int labelsColor)
-    {// 设置主描绘器的各项属性，详情可阅读官方API文档
-        renderer.setChartTitle(title);//图的标题
-        renderer.setXTitle(xTitle);//图的x轴标题
-        renderer.setYTitle(yTitle);//图的y轴标题
-        renderer.setXAxisMin(xMin);//x轴最小值
-        renderer.setXAxisMax(xMax);//x轴最大值
-        renderer.setYAxisMin(yMin);//y轴最小值
-        renderer.setYAxisMax(yMax);//y轴最大值
-        renderer.setAxesColor(axesColor);//轴的颜色
-        renderer.setLabelsColor(labelsColor);//label的颜色
-    }
 }
---
+++ app/src/main/java/james/com/mag1c_band/ChartFile/ChartService.java
@@ -0,0 +1,147 @@
+package james.com.mag1c_band.ChartFile;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint.Align;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
+import java.util.List;
+
+public class ChartService {
+
+    private GraphicalView mGraphicalView;
+    private XYMultipleSeriesDataset multipleSeriesDataset;// 数据集容器
+    private XYMultipleSeriesRenderer multipleSeriesRenderer;// 渲染器容器
+    private XYSeries mSeries;// 单条曲线数据集
+    private XYSeriesRenderer mRenderer;// 单条曲线渲染器
+    private Context context;
+    public static int Xmin = 0;
+    public static int Xmax = 100;
+
+    public ChartService(Context context) {
+        this.context = context;
+    }
+
+    /**
+     * 获取图表
+     *
+     * @return
+     */
+    public GraphicalView getGraphicalView() {
+        mGraphicalView = ChartFactory.getCubeLineChartView(context,
+                multipleSeriesDataset, multipleSeriesRenderer, 0.1f);
+        return mGraphicalView;
+    }
+
+    /**
+     * 获取数据集，及xy坐标的集合
+     *
+     * @param curveTitle
+     */
+    public void setXYMultipleSeriesDataset(String curveTitle) {
+        multipleSeriesDataset = new XYMultipleSeriesDataset();
+        mSeries = new XYSeries(curveTitle);
+        multipleSeriesDataset.addSeries(mSeries);
+    }
+
+    /**
+     * 获取渲染器
+     *
+     * @param maxX
+     *            x轴最大值
+     * @param maxY
+     *            y轴最大值
+     * @param chartTitle
+     *            曲线的标题
+     * @param xTitle
+     *            x轴标题
+     * @param yTitle
+     *            y轴标题
+     * @param axeColor
+     *            坐标轴颜色
+     * @param labelColor
+     *            标题颜色
+     * @param curveColor
+     *            曲线颜色
+     * @param gridColor
+     *            网格颜色
+     */
+    public void setXYMultipleSeriesRenderer(double maxX, double maxY,
+                                            String chartTitle, String xTitle, String yTitle, int axeColor,
+                                            int labelColor, int curveColor, int gridColor) {
+        multipleSeriesRenderer = new XYMultipleSeriesRenderer();
+        if (chartTitle != null) {
+            multipleSeriesRenderer.setChartTitle(chartTitle);
+        }
+        multipleSeriesRenderer.setPanEnabled(true,false);//x轴可以平移而y轴不可以
+        multipleSeriesRenderer.setXTitle(xTitle);
+        multipleSeriesRenderer.setYTitle(yTitle);
+        multipleSeriesRenderer.setRange(new double[]{0, maxX, 0, maxY});//xy轴的范围
+        multipleSeriesRenderer.setLabelsColor(labelColor);
+        multipleSeriesRenderer.setXAxisMin(Xmin);
+        multipleSeriesRenderer.setXAxisMax(Xmax);
+        multipleSeriesRenderer.setXLabels(10);
+        multipleSeriesRenderer.setYLabels(10);
+        multipleSeriesRenderer.setXLabelsAlign(Align.RIGHT);
+        multipleSeriesRenderer.setYLabelsAlign(Align.RIGHT);
+        multipleSeriesRenderer.setAxisTitleTextSize(30);
+        multipleSeriesRenderer.setChartTitleTextSize(30);
+        multipleSeriesRenderer.setLabelsTextSize(30);
+        multipleSeriesRenderer.setLegendTextSize(20);
+        multipleSeriesRenderer.setPointSize(10f);//曲线描点尺寸
+        multipleSeriesRenderer.setFitLegend(true);
+        multipleSeriesRenderer.setMargins(new int[]{20, 30, 15, 20});
+        multipleSeriesRenderer.setShowGrid(true);
+        multipleSeriesRenderer.setZoomEnabled(true, false);
+        multipleSeriesRenderer.setAxesColor(axeColor);
+        multipleSeriesRenderer.setGridColor(gridColor);
+        multipleSeriesRenderer.setDisplayValues(true);
+        multipleSeriesRenderer.setBackgroundColor(Color.WHITE);//背景色
+        multipleSeriesRenderer.setMarginsColor(Color.WHITE);//边距背景色，默认背景色为黑色，这里修改为白色
+        mRenderer = new XYSeriesRenderer();
+        mRenderer.setColor(curveColor);
+        mRenderer.setPointStyle(PointStyle.CIRCLE);//描点风格，可以为圆点，方形点等等
+        mRenderer.setFillPoints(true);
+        multipleSeriesRenderer.addSeriesRenderer(mRenderer);
+    }
+
+    /**
+     * 根据新加的数据，更新曲线，只能运行在主线程
+     *
+     * @param x
+     *            新加点的x坐标
+     * @param y
+     *            新加点的y坐标
+     */
+    public void updateChart(double x, double y) {
+        multipleSeriesDataset.removeSeries(mSeries);
+        mSeries.add(x, y);
+        multipleSeriesDataset.addSeries(0,mSeries);
+        //Xmin += 5;
+        //Xmax += 5;
+        if (Xmin + x >= 90){//当图还有百分之十的空闲时开始平移背景
+            multipleSeriesRenderer.setXAxisMin(Xmin + x - 80);//始终留有百分之十的空余
+            multipleSeriesRenderer.setXAxisMax(Xmax + x - 80);
+        }
+        mGraphicalView.repaint();//此处也可以调用invalidate()
+    }
+
+    /**
+     * 添加新的数据，多组，更新曲线，只能运行在主线程
+     * @param xList
+     * @param yList
+     */
+    public void updateChart(List<Double> xList, List<Double> yList) {
+        for (int i = 0; i < xList.size(); i++) {
+            mSeries.add(xList.get(i), yList.get(i));
+        }
+        mGraphicalView.repaint();//此处也可以调用invalidate()
+    }
+}
