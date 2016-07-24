1ad0577e2cc99cc4012f9abc81c9266ead20aade
James <caterpillarous@yahoo.com>
2016-07-24 22:08:11 +0800

    to the public net

--- app/src/main/java/james/com/mag1c_band/Data/URL.java
+++ app/src/main/java/james/com/mag1c_band/Data/URL.java
@@ -1,6 +1,6 @@
 package james.com.mag1c_band.Data;
 
 public class URL {
-    public static final String LOGIN_URL = "http://10.3.116.176:8000/login/";
-    public static final String REGISTER_URL = "http://10.3.116.146:8000/band_register/";
+    public static final String LOGIN_URL = "http://123.206.29.111/login/";
+    public static final String REGISTER_URL = "http://123.206.29.111/band_register/";
 }
